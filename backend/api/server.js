e03a82db26be28e784f7ee9e636eb389fb94fccb
Fernando Ribeiro Aguilar <fernando.aguilar@hotmail.com.br>
2021-05-26 21:43:45 -0400

    [TDD] Add initial test cases

--- backend/api/server.js
+++ backend/api/server.js
@@ -18,11 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to application." });
+  res.json({ msg: "Welcome to application." });
 });
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+module.exports = app;
--- backend/tests/unit.test.js
+++ backend/tests/unit.test.js
@@ -1,6 +1,18 @@
-describe('Sample Test', () => {
+const request = require("supertest");
+const app = require("../api/server");
+
+describe('Litmus Test', () => {
     it('should test that true === true', () => {
       expect(true).toBe(true)
     })
   })
   
+
+describe('Test Base API endpoint', () => {
+  it('should test that endpoint is reached', async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("msg");
+    expect(res.body.msg).toEqual("Welcome to application.");  
+  })
+})
