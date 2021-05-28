683b356e2924131a622759dd813afad2199fba35
Fernando Ribeiro Aguilar <fernando.aguilar@hotmail.com.br>
2021-05-28 16:02:46 -0400

    [Feat]: add README & cpf validator in customer.controller

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # voice
+## How to run
+
+> run frontend with react
+```
+cd frontend/
+yarn install
+yarn start
+```
+
+
+> run server with node.js
+* required to install node and mongodb-community locally
+
+
+<!-- (alterantively run mongo db with `docker-compose up`) -->
+
+```
+cd backend/
+yarn install
+yarn serve
+```
+## Run unit tests
+> yarn tests
+
--- backend/api/controllers/customer.controller.js
+++ backend/api/controllers/customer.controller.js
@@ -1,14 +1,54 @@
 const db = require("../config");
 const Customer = db.customers;
 
-const validateCPF = (cpf) => {
-  return true;
-};
+function ValidateCPF(cpfString) {
+    let validated = false; // Start with false
 
-// Create and Save a new Customer
+    // Checking cpf lenght
+    if (cpfString.length !== 14 && cpfString.length !== 11) {
+      return validated;
+    }
+
+    // Checking for "formated cpf (000.000.000-00)" and replace "points"
+    if (cpfString.length === 14) {
+      cpfString = cpfString.replace(".", "");
+      cpfString = cpfString.replace(".", "");
+      cpfString = cpfString.replace("-", "");
+    }
+
+    // Variables to check cpf valid (sum and rest)
+    let sum = 0,
+      rest = 0;
+
+    // Checking for "null" CPF
+    if (cpfString === "00000000000") {
+      return validated;
+    }
+
+    // sum numbers
+    for (let i = 1; i <= 9; ++i) {
+      sum += parseInt(cpfString.substring(i - 1, i)) * (11 - i);
+    }
+
+    // Getting rest
+    rest = (sum * 10) % 11;
+
+    if (rest == 10 || rest == 11) {
+      rest = 0;
+    }
+
+    if (rest != parseInt(cpfString.substring(9, 10))) {
+      return validated;
+    }
+
+    validated = true;
+    return validated;
+  }
+
+  // Create and Save a new Customer
 exports.create = (req, res) => {
   // Validate request
-  if (!validateCPF(req.body.cpf)) {
+  if (!ValidateCPF(req.body.cpf)) {
     res.status(400).send({ success: false, msg: "CPF inválido." });
     return;
   }
